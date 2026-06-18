095ea9fc263cedf6d68805a2f65317eb6ae86f4c
seokkak <seok7184@gmail.com>
2026-06-18 17:41:14 +0900

    SecurityConfig

---
+++ src/main/java/com/example/springedu2/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.springedu2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+// SpringSecurity 의 설정
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/", "/index.html", "/css/**", "/img/**", "/js/**", "/fonts/**", "/login", "/member/register")
+                        .permitAll() // 로그인없이 사용가능
+                        .requestMatchers("/admin/**", "/vupdate", "/vdelete").hasRole("ADMIN")
+                        .requestMatchers("/visitorMain.html", "visitorForm.html", "/vlist", "vinsert", "vsearch", "/one", "/member/me").authenticated() // 로그인 필요
+                        .anyRequest().authenticated() // 설정하지 않은 다른 요청도 로그인 필요
+                )
+                .formLogin(form->form.loginPage("/login"))
+                .logout(logout->logout.logoutUrl("/logout"))
+                .exceptionHandling(exception -> exception.accessDeniedPage("/access-denied"));
+        return http.build();
+    }
+
+    // 비밀번호 암호화
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
